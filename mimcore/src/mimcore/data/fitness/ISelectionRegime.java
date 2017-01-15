2f5e40b3a1970d5b86e377c85c74b51c3bad0df4
Robert Kofler <rokofler@gmail.com>
2017-01-15 15:46:06 +0000

    lots of changes ro

--- mimcore/src/mimcore/data/fitness/ISelectionRegime.java
+++
@@ -1,9 +0,0 @@
-package qmimcore.data.fitness;
-
-/**
- * Created by robertkofler on 7/16/14.
- */
-public interface ISelectionRegime {
-
-	public abstract double getSelectionIntensity(int generation, int replicate);
-}
